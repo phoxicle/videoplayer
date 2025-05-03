4a252e3c953055153ed6c0f1708b092af47ee227
Chris Gerpheide <chris.ger@phei.de>
2025-05-03 11:56:28 +0900

    Support gradlew run

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 
 plugins {
+    application
     kotlin("jvm") version "2.1.20"
     kotlin("plugin.serialization") version "2.1.20"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("MainKt")
+}
 
 repositories {
     mavenCentral()
